50ef9f3647daffd8e2b4833eee5eacd6678d47ff
Abdelrahman <86893144+abdelrahamn1@users.noreply.github.com>
2023-10-31 02:45:58 +0200

    Update reg.js

--- Appjs/reg.js
+++ Appjs/reg.js
@@ -19,38 +19,75 @@ const stepFour = document.querySelector(".formbold-form-step-4");
 // Form buttons
 const formSubmitBtn = document.querySelector(".formbold-btn");
 const formBackBtn = document.querySelector(".formbold-back-btn");
+const input = document.querySelectorAll(".formbold-form-input");
+const selectinput = document.querySelectorAll("select");
+console.log(selectinput[0].value);
+
+function areRequiredFieldsFilled(step) {
+  const requiredInputs = step.querySelectorAll("input[required]");
+  for (const input of requiredInputs) {
+    if (input.value.trim() === "") {
+      return false;
+    }
+  }
+  return true;
+}
 
 formSubmitBtn.addEventListener("click", function (event) {
   event.preventDefault();
   if (stepMenuOne.className == "formbold-step-menu1 active") {
-    // event.preventDefault();
+    if (areRequiredFieldsFilled(stepOne)) {
+      event.preventDefault();
+      stepMenuOne.classList.remove("active");
+      stepMenuTwo.classList.add("active");
+      stepOne.classList.remove("active");
+      stepTwo.classList.add("active");
+      // back to stage 1
+      formBackBtn.classList.add("active");
+    } else {
+      alert("Please fill empty fields");
+    }
     // go to stage 2 ---- stage2 is now active
-    stepMenuOne.classList.remove("active");
-    stepMenuTwo.classList.add("active");
-    stepOne.classList.remove("active");
-    stepTwo.classList.add("active");
-    // back to stage 1
-    formBackBtn.classList.add("active");
   } else if (stepMenuTwo.className == "formbold-step-menu2 active") {
     event.preventDefault();
-    // go to stage 3 --- stage3 is now active
-    stepMenuTwo.classList.remove("active");
-    stepMenuThree.classList.add("active");
-    stepTwo.classList.remove("active");
-    stepThree.classList.add("active");
-    //back to stage 2
-    formBackBtn.classList.add("active");
+    if (
+      areRequiredFieldsFilled(stepTwo) &&
+      selectinput[0].value != 0 &&
+      selectinput[1].value != 0
+    ) {
+      // go to stage 3 --- stage3 is now active
+      stepMenuTwo.classList.remove("active");
+      stepMenuThree.classList.add("active");
+      stepTwo.classList.remove("active");
+      stepThree.classList.add("active");
+      //back to stage 2
+      formBackBtn.classList.add("active");
+    } else {
+      alert("Please fill empty fields");
+    }
   } else if (stepMenuThree.className == "formbold-step-menu3 active") {
     event.preventDefault();
-    // go to stage 4 , ----- stage 4 is now active
-    stepMenuThree.classList.remove("active");
-    stepMenuFour.classList.add("active");
-    stepThree.classList.remove("active");
-    stepFour.classList.add("active");
-    formSubmitBtn.textContent = "Submit";
-    //back to stage 3
-    formBackBtn.classList.add("active");
+    if (areRequiredFieldsFilled(stepThree)) {
+      // go to stage 4 , ----- stage 4 is now active
+      stepMenuThree.classList.remove("active");
+      stepMenuFour.classList.add("active");
+      stepThree.classList.remove("active");
+      stepFour.classList.add("active");
+      formSubmitBtn.textContent = "Submit";
+      //back to stage 3
+      formBackBtn.classList.add("active");
+    } else {
+      alert("Please fill empty fields");
+    }
   } else if (stepMenuFour.className == "formbold-step-menu4 active") {
+    if (areRequiredFieldsFilled(stepFour) && selectinput[2].value != 0) {
+      if (YesAnswer.checked) {
+      }
+      // document.querySelector("form").submit();
+      console.log("lol");
+    } else {
+      alert("Please fill empty fields");
+    }
     // document.querySelector("form").submit();
   }
 });
@@ -89,7 +126,7 @@ const GraduationYear = document.querySelector(".year-graduation");
 YesAnswer.onclick = function () {
   Acadmic.style.display = "block";
   GraduationYear.style.display = "block";
-  faculty.style.display = "none";
+  faculty.style.display = "block";
 };
 NoAnswer.onclick = function () {
   Acadmic.style.display = "block";
