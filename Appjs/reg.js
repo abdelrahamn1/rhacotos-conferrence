3efdad1cc7f486ccf3489778550f4d2225546860
Abdelrahman <86893144+abdelrahamn1@users.noreply.github.com>
2023-11-02 16:59:11 +0200

    Update reg.js

--- Appjs/reg.js
+++ Appjs/reg.js
@@ -1,11 +1,3 @@
-//---------------------------INPUT FILED ACCEPT ONLY NUMBER-------------------------------- //
-//
-function isNumberKey(event) {
-  var charCode = event.which ? event.which : event.keyCode;
-  if (charCode > 31 && (charCode < 48 || charCode > 57)) return false;
-  return true;
-}
-// -----------------------------Form-------------------------------------//
 // Form "li" stages
 const stepMenuOne = document.querySelector(".formbold-step-menu1");
 const stepMenuTwo = document.querySelector(".formbold-step-menu2");
@@ -21,22 +13,33 @@ const formSubmitBtn = document.querySelector(".formbold-btn");
 const formBackBtn = document.querySelector(".formbold-back-btn");
 const input = document.querySelectorAll(".formbold-form-input");
 const selectinput = document.querySelectorAll("select");
-console.log(selectinput[0].value);
-
-function areRequiredFieldsFilled(step) {
-  const requiredInputs = step.querySelectorAll("input[required]");
-  for (const input of requiredInputs) {
-    if (input.value.trim() === "") {
-      return false;
-    }
-  }
+// Modal
+let Modal = document.getElementById("myModal");
+let CloseModal = document.getElementById("closeModal");
+// ckeck Radio , inputs step 4 fileds
+const YesAnswer = document.querySelector("#yes-qual");
+const NoAnswer = document.querySelector("#no-qual");
+const Acadmic = document.querySelector(".Acadmic");
+const faculty = document.querySelector(".faculty");
+const GraduationYear = document.querySelector(".year-graduation");
+const YearStudy = document.querySelector(".year-study");
+//---------------------------INPUT FILED ACCEPT ONLY NUMBER-------------------------------- //
+function isNumberKey(event) {
+  var charCode = event.which ? event.which : event.keyCode;
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) return false;
   return true;
 }
-
+// -----------------------------Form-------------------------------------//
 formSubmitBtn.addEventListener("click", function (event) {
   event.preventDefault();
   if (stepMenuOne.className == "formbold-step-menu1 active") {
-    if (areRequiredFieldsFilled(stepOne)) {
+    if (
+      input[0].value.trim() !== "" &&
+      input[1].value.trim() !== "" &&
+      input[2].value.trim() !== "" &&
+      input[3].value.trim() !== "" &&
+      input[4].value.trim() !== ""
+    ) {
       event.preventDefault();
       stepMenuOne.classList.remove("active");
       stepMenuTwo.classList.add("active");
@@ -45,13 +48,14 @@ formSubmitBtn.addEventListener("click", function (event) {
       // back to stage 1
       formBackBtn.classList.add("active");
     } else {
-      alert("Please fill empty fields");
+      openModal();
     }
     // go to stage 2 ---- stage2 is now active
   } else if (stepMenuTwo.className == "formbold-step-menu2 active") {
     event.preventDefault();
     if (
-      areRequiredFieldsFilled(stepTwo) &&
+      input[5].value.trim() !== "" &&
+      input[6].value.trim() !== "" &&
       selectinput[0].value != 0 &&
       selectinput[1].value != 0
     ) {
@@ -63,11 +67,16 @@ formSubmitBtn.addEventListener("click", function (event) {
       //back to stage 2
       formBackBtn.classList.add("active");
     } else {
-      alert("Please fill empty fields");
+      openModal();
     }
   } else if (stepMenuThree.className == "formbold-step-menu3 active") {
     event.preventDefault();
-    if (areRequiredFieldsFilled(stepThree)) {
+    if (
+      input[7].value.trim() !== "" &&
+      input[8].value.trim() !== "" &&
+      input[9].value.trim() !== "" &&
+      input[10].value.trim() !== ""
+    ) {
       // go to stage 4 , ----- stage 4 is now active
       stepMenuThree.classList.remove("active");
       stepMenuFour.classList.add("active");
@@ -77,21 +86,28 @@ formSubmitBtn.addEventListener("click", function (event) {
       //back to stage 3
       formBackBtn.classList.add("active");
     } else {
-      alert("Please fill empty fields");
+      openModal();
     }
   } else if (stepMenuFour.className == "formbold-step-menu4 active") {
-    if (areRequiredFieldsFilled(stepFour) && selectinput[2].value != 0) {
-      if (YesAnswer.checked) {
+    if (YesAnswer.checked) {
+      if (
+        input[11].value.trim() !== "" &&
+        input[13].value.trim() !== "" &&
+        selectinput[2].value != 0
+      ) {
+        document.querySelector("form").submit();
+      } else {
+        openModal();
       }
-      // document.querySelector("form").submit();
-      console.log("lol");
-    } else {
-      alert("Please fill empty fields");
+    } else if (NoAnswer.checked) {
+      if (input[11].value.trim() !== "" && input[12].value.trim() !== "") {
+        document.querySelector("form").submit();
+      } else {
+        openModal();
+      }
     }
-    // document.querySelector("form").submit();
   }
 });
-
 // -----------------------------Form Back Button-------------------------------------//
 formBackBtn.onclick = function (e) {
   e.preventDefault();
@@ -117,20 +133,24 @@ formBackBtn.onclick = function (e) {
   }
 };
 // -------------------------Acadmic--------------------------------------------------//
-const YesAnswer = document.querySelector("#yes-qual");
-const NoAnswer = document.querySelector("#no-qual");
-const Acadmic = document.querySelector(".Acadmic");
-const faculty = document.querySelector(".faculty");
-const GraduationYear = document.querySelector(".year-graduation");
-
 YesAnswer.onclick = function () {
   Acadmic.style.display = "block";
   GraduationYear.style.display = "block";
   faculty.style.display = "block";
+  YearStudy.style.display = "none";
 };
 NoAnswer.onclick = function () {
-  Acadmic.style.display = "block";
-  GraduationYear.style.display = "block";
+  YearStudy.style.display = "block";
+  Acadmic.style.display = "none";
+  GraduationYear.style.display = "none";
   faculty.style.display = "block";
 };
-// -----------------------------------------------------------------------------------//
+// ---------------------------------------Modal---------------------------------------//
+
+function openModal() {
+  Modal.style.display = "block";
+}
+// Close the custom modal dialog
+CloseModal.addEventListener("click", function () {
+  Modal.style.display = "none";
+});
