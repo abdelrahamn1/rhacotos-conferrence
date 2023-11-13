92a060d6570368ab7069afb2cc01cfb9edd09ecf
Abdelrahman <86893144+abdelrahamn1@users.noreply.github.com>
2023-11-13 22:50:18 +0200

    Update reg.js

--- Appjs/reg.js
+++ Appjs/reg.js
@@ -21,6 +21,9 @@ let CloseModal = document.getElementById("closeModal");
 // ckeck Radio , inputs step 4 fileds
 const YesAnswer = document.querySelector("#yes-qual");
 const NoAnswer = document.querySelector("#no-qual");
+const NoAnswerWork = document.querySelector("#no-qual-work");
+const yesAnswerWork = document.querySelector("#yes-qual-work");
+const Company = document.querySelector(".company-name");
 const Acadmic = document.querySelector(".Acadmic");
 const faculty = document.querySelector(".faculty");
 const GraduationYear = document.querySelector(".year-graduation");
@@ -28,6 +31,7 @@ const YearStudy = document.querySelector(".year-study");
 const university = document.querySelector(".university");
 const yearOFHighest = document.querySelector(".year-of-highest");
 const H4faculty = document.querySelector(".faculty h4");
+console.log(input);
 //---------------------------INPUT FILED ACCEPT ONLY NUMBER-------------------------------- //
 function isNumberKey(event) {
   var charCode = event.which ? event.which : event.keyCode;
@@ -43,7 +47,8 @@ formSubmitBtn.addEventListener("click", function (event) {
       input[1].value.trim() !== "" &&
       input[2].value.trim() !== "" &&
       input[3].value.trim() !== "" &&
-      input[4].value.trim() !== ""
+      input[4].value.trim() !== "" &&
+      input[5].value.trim() !== ""
     ) {
       event.preventDefault();
       stepMenuOne.classList.remove("active");
@@ -59,8 +64,8 @@ formSubmitBtn.addEventListener("click", function (event) {
   } else if (stepMenuTwo.className == "formbold-step-menu2 active") {
     event.preventDefault();
     if (
-      input[5].value.trim() !== "" &&
       input[6].value.trim() !== "" &&
+      input[7].value.trim() !== "" &&
       selectinput[0].value != 0 &&
       selectinput[1].value != 0
     ) {
@@ -77,10 +82,10 @@ formSubmitBtn.addEventListener("click", function (event) {
   } else if (stepMenuThree.className == "formbold-step-menu3 active") {
     event.preventDefault();
     if (
-      input[7].value.trim() !== "" &&
       input[8].value.trim() !== "" &&
       input[9].value.trim() !== "" &&
-      input[10].value.trim() !== ""
+      input[10].value.trim() !== "" &&
+      input[11].value.trim() !== ""
     ) {
       // go to stage 4 , ----- stage 4 is now active
       stepMenuThree.classList.remove("active");
@@ -95,29 +100,43 @@ formSubmitBtn.addEventListener("click", function (event) {
   } else if (stepMenuFour.className == "formbold-step-menu4 active") {
     if (YesAnswer.checked) {
       if (
-        input[11].value.trim() !== "" &&
-        input[12].value.trim() !== "" &&
         input[13].value.trim() !== "" &&
         input[14].value.trim() !== "" &&
+        input[15].value.trim() !== "" &&
+        input[16].value.trim() !== "" &&
         selectinput[2].value != 0
       ) {
-        stepMenuFour.classList.remove("active");
-        stepMenuFive.classList.add("active");
-        stepFour.classList.remove("active");
-        stepFive.classList.add("active");
-        formSubmitBtn.textContent = "Submit";
-        //back to stage 4
-        formBackBtn.classList.add("active");
+        if (Company.style.display === "block") {
+          if (input[12].value.trim() !== "") {
+            stepMenuFour.classList.remove("active");
+            stepMenuFive.classList.add("active");
+            stepFour.classList.remove("active");
+            stepFive.classList.add("active");
+            formSubmitBtn.textContent = "Submit";
+            //back to stage 4
+            formBackBtn.classList.add("active");
+          } else {
+            openModal();
+          }
+        } else if (Company.style.display === "none") {
+          stepMenuFour.classList.remove("active");
+          stepMenuFive.classList.add("active");
+          stepFour.classList.remove("active");
+          stepFive.classList.add("active");
+          formSubmitBtn.textContent = "Submit";
+          //back to stage 4
+          formBackBtn.classList.add("active");
+        }
       } else {
         openModal();
       }
     } else if (NoAnswer.checked) {
       if (
-        input[11].value.trim() !== "" &&
-        input[12].value.trim() !== "" &&
         input[13].value.trim() !== "" &&
         input[14].value.trim() !== "" &&
         input[15].value.trim() !== "" &&
+        input[16].value.trim() !== "" &&
+        input[17].value.trim() !== "" &&
         selectinput[2].value != 0
       ) {
         stepMenuFour.classList.remove("active");
@@ -132,7 +151,7 @@ formSubmitBtn.addEventListener("click", function (event) {
       }
     }
   } else if (stepMenuFive.className == "formbold-step-menu5 active") {
-    if (input[16].value.trim() !== "") {
+    if (input[17].value.trim() !== "") {
       document.querySelector("form").submit();
     } else {
       openModal();
@@ -184,6 +203,12 @@ NoAnswer.onclick = function () {
   university.style.display = "block";
   YearStudy.style.display = "block";
   GraduationYear.style.display = "block";
+};
+yesAnswerWork.onclick = function () {
+  Company.style.display = "block";
+};
+NoAnswerWork.onclick = function () {
+  Company.style.display = "none";
 };
 // ---------------------------------------Modal---------------------------------------//
 
